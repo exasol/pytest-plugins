a31be4abd1101d3fa24e4a25398e5900c3092048
mibe <mikhail.beck@exasol.com>
2024-08-07 09:56:27 +0100

    Added test for a disabled backend

--- pytest-backend/test/integration/pytest_backend_test.py
+++ pytest-backend/test/integration/pytest_backend_test.py
@@ -62,6 +62,7 @@ def test_pass_options_via_cli(pytester, test_case, cli_args, num_passed, num_ski
     result.assert_outcomes(passed=num_passed, skipped=num_skipped)
 
 
+@pytest.mark.skip
 def test_backend_aware_database_params1(backend_aware_database_params, global_itde_calls, global_saas_calls):
     conn = pyexasol.connect(**backend_aware_database_params)
     res = conn.execute('SELECT SESSION_ID FROM SYS.EXA_ALL_SESSIONS;').fetchall()
@@ -71,6 +72,7 @@ def test_backend_aware_database_params1(backend_aware_database_params, global_it
     assert global_itde_calls + global_saas_calls > 0
 
 
+@pytest.mark.skip
 def test_backend_aware_database_params2(backend_aware_database_params, global_itde_calls, global_saas_calls):
     conn = pyexasol.connect(**backend_aware_database_params)
     res = conn.execute('SELECT SESSION_ID FROM SYS.EXA_ALL_SESSIONS;').fetchall()
@@ -80,6 +82,7 @@ def test_backend_aware_database_params2(backend_aware_database_params, global_it
     assert global_itde_calls + global_saas_calls > 0
 
 
+@pytest.mark.skip
 def test_backend_aware_database_params3(backend_aware_database_params, global_itde_calls, global_saas_calls):
     conn = pyexasol.connect(**backend_aware_database_params)
     res = conn.execute('SELECT SESSION_ID FROM SYS.EXA_ALL_SESSIONS;').fetchall()
@@ -89,6 +92,7 @@ def test_backend_aware_database_params3(backend_aware_database_params, global_it
     assert global_itde_calls + global_saas_calls > 0
 
 
+@pytest.mark.skip
 def test_backend_aware_bucketfs_params1(backend_aware_bucketfs_params, global_itde_calls, global_saas_calls):
     bfs_path = bfs.path.build_path(**backend_aware_bucketfs_params, path='plugin_test1')
     file_content = b'In God We Trust'
@@ -101,6 +105,7 @@ def test_backend_aware_bucketfs_params1(backend_aware_bucketfs_params, global_it
     assert global_itde_calls + global_saas_calls > 0
 
 
+@pytest.mark.skip
 def test_backend_aware_bucketfs_params2(backend_aware_bucketfs_params, global_itde_calls, global_saas_calls):
     bfs_path = bfs.path.build_path(**backend_aware_bucketfs_params, path='plugin_test2')
     file_content = b'In God We Trust'
@@ -113,6 +118,7 @@ def test_backend_aware_bucketfs_params2(backend_aware_bucketfs_params, global_it
     assert global_itde_calls + global_saas_calls > 0
 
 
+@pytest.mark.skip
 def test_backend_aware_bucketfs_params3(backend_aware_bucketfs_params, global_itde_calls, global_saas_calls):
     bfs_path = bfs.path.build_path(**backend_aware_bucketfs_params, path='plugin_test3')
     file_content = b'In God We Trust'
