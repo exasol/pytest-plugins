19d9646025b2616b6b1cf3387bb5af75ab7ff211
mibe <mikhail.beck@exasol.com>
2024-08-23 14:57:34 +0100

    #49 Made a proper integration test

--- pytest-slc/test/integration/pytest_slc_test.py
+++ pytest-slc/test/integration/pytest_slc_test.py
@@ -1,13 +1,27 @@
-import textwrap
+from textwrap import dedent
 import pytest
-import pyexasol
+from exasol.pytest_backend import (BACKEND_OPTION, BACKEND_ALL)
 
+pytest_plugins = ["pytester"]
+
+_udf_create_sql = dedent("""
+    CREATE OR REPLACE {LANGUAGE_ALIAS} SCALAR SCRIPT "{schema}"."{udf_name}"()
+        RETURNS BOOLEAN AS
+        def run(ctx):
+            return True
+        /
+""")
+
+_udf_call_sql = """SELECT "{schema}"."{udf_name}"()"""
+
+_test_code = dedent(f"""
+import pyexasol
+import pytest
 from exasol.python_extension_common.deployment.language_container_validator import temp_schema
 from exasol.python_extension_common.deployment.language_container_builder import (
     LanguageContainerBuilder, find_path_backwards)
 
 LANGUAGE_ALIAS = 'PYTHON3_PYTEST_SLC'
-
 
 @pytest.fixture(scope='session', autouse=True)
 def extension_build_slc_async(export_slc_async):
@@ -16,26 +30,24 @@ def extension_build_slc_async(export_slc_async):
         slc_builder.prepare_flavor(project_directory)
         yield export_slc_async(slc_builder)
 
-
 @pytest.fixture(scope='session')
 def extension_upload_slc(extension_build_slc_async, upload_slc):
     upload_slc(*extension_build_slc_async, 'container')
 
-
 def assert_udf_running(conn: pyexasol.ExaConnection):
     with temp_schema(conn) as schema:
         udf_name = 'TEST_UDF'
-        conn.execute(textwrap.dedent(f"""
-            CREATE OR REPLACE {LANGUAGE_ALIAS} SCALAR SCRIPT "{schema}"."{udf_name}"()
-            RETURNS BOOLEAN AS
-            def run(ctx):
-                return True
-            /
-            """))
-        result = conn.execute(f'SELECT "{schema}"."{udf_name}"()').fetchall()
+        conn.execute(f'{_udf_call_sql}')
+        result = conn.execute(f'{_udf_call_sql}').fetchall()
         assert result[0][0] is True
 
-
 def test_upload_slc(extension_upload_slc, backend_aware_database_params):
-    if extension_upload_slc:
-        assert_udf_running(pyexasol.connect(**backend_aware_database_params))
+    assert_udf_running(pyexasol.connect(**backend_aware_database_params))
+""")
+
+
+def test_pytest_slc(pytester):
+    pytester.makepyfile(_test_code)
+    result = pytester.runpytest(BACKEND_OPTION, BACKEND_ALL)
+    assert result.ret == pytest.ExitCode.OK
+    result.assert_outcomes(passed=2, skipped=0)
