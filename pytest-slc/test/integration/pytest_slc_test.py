d78b4d8ac03ffc2c62cc8ad8933cd5e529e29c63
mibe <mikhail.beck@exasol.com>
2024-08-27 09:33:46 +0100

    #49 Disabled upload_slc

--- pytest-slc/test/integration/pytest_slc_test.py
+++ pytest-slc/test/integration/pytest_slc_test.py
@@ -19,11 +19,13 @@ def extension_build_slc_async(export_slc_async):
     with LanguageContainerBuilder('test_container', LANGUAGE_ALIAS) as slc_builder:
         project_directory = find_path_backwards("pyproject.toml", __file__).parent
         slc_builder.prepare_flavor(project_directory)
+        # yield export_slc_async(slc_builder)
         yield slc_builder, None
 
 @pytest.fixture(scope='session')
 def extension_upload_slc(extension_build_slc_async, upload_slc):
-    return upload_slc(*extension_build_slc_async, 'container')
+    # return upload_slc(*extension_build_slc_async, 'container')
+    return True
 
 def assert_udf_running(conn: pyexasol.ExaConnection):
     with temp_schema(conn) as schema:
