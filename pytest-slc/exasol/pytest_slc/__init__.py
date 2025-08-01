707b349cc9ca987b8cd12112787a6a8d5150f6a1
ckunki <christoph.kuhnke@exasol.com>
2025-08-01 13:43:30 +0200

    project:fix

--- pytest-slc/exasol/pytest_slc/__init__.py
+++ pytest-slc/exasol/pytest_slc/__init__.py
@@ -56,7 +56,7 @@ def export_slc_async(
     slc_builder: LanguageContainerBuilder,
     use_onprem: bool,
     use_saas: bool,
-): # -> Iterator[ExportContainerResult]:
+):  # -> Iterator[ExportContainerResult]:
     """
     The fixture starts the export() function of the provided
     LanguageContainerBuilder object as an asynchronous task.
