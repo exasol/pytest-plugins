8a249217de6e81cd43fadcb6fe93b33d0e609d75
ckunki <christoph.kuhnke@exasol.com>
2026-02-10 17:44:39 +0100

    Updated PTB to version 5

--- pytest-slc/noxconfig.py
+++ pytest-slc/noxconfig.py
@@ -1,49 +1,9 @@
-"""Configuration for nox based task runner"""
-
-from __future__ import annotations
-
-from collections.abc import (
-    Iterable,
-    MutableMapping,
-)
 from pathlib import Path
-from typing import (
-    Any,
-)
 
 from exasol.toolbox.config import BaseConfig
-from nox import Session
 
-
-class Config(BaseConfig):
-    """Project specific configuration used by nox infrastructure"""
-
-    root: Path = Path(__file__).parent
-    doc: Path = Path(__file__).parent / "doc"
-    version_file: Path = Path(__file__).parent / "exasol" / "pytest_slc" / "version.py"
-    path_filters: Iterable[str] = ("dist", ".eggs", "venv", "metrics-schema", ".poetry")
-    source: Path = Path("exasol/pytest_slc")
-
-    @staticmethod
-    def pre_integration_tests_hook(
-        _session: Session, _config: Config, _context: MutableMapping[str, Any]
-    ) -> bool:
-        """Implement if project specific behaviour is required"""
-        return True
-
-    @staticmethod
-    def post_integration_tests_hook(
-        _session: Session, _config: Config, _context: MutableMapping[str, Any]
-    ) -> bool:
-        """Implement if project specific behaviour is required"""
-        return True
-
-
-PROJECT_CONFIG = Config(
-    # Restrict Python versions as difficulty compiling dependencies for Python 3.14
-    # This will be resolved in:
-    #    https://github.com/exasol/pytest-plugins/issues/132
+PROJECT_CONFIG = BaseConfig(
+    root_path=Path(__file__).parent,
+    project_name="pytest_slc",
     python_versions=("3.10", "3.11", "3.12", "3.13"),
-    # Uses SaaS; not ITDE DB versions
-    exasol_versions=(),
 )
