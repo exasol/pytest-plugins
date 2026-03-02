1f3e91dcaa538de2121d350dc46d06a40fb5d97b
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-03-02 16:34:40 +0100

    Remove pytest-backend (fixes #174)
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- pytest-backend/doc/changes/changes_0.3.3.md
+++
@@ -1,9 +0,0 @@
-# 0.3.3 - 2025-02-11
-
-## Summary
-
-Dependency update on top of 0.3.2
-
-## Internal
-
-* Updated exasol-bucketfs to >=1.0.0
--- pytest-backend/noxfile.py
+++
@@ -1,7 +0,0 @@
-import nox
-
-# imports all nox task provided by the toolbox
-from exasol.toolbox.nox.tasks import *  # noqa: F403, pylint: disable=wildcard-import disable=unused-wildcard-import
-
-# default actions to be run if nothing is explicitly specified with the -s option
-nox.options.sessions = ["format:fix"]
